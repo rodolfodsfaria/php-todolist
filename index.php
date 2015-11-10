e28ca599b8ec08a2e6c3faa27fc7793415d400de
rodolfodsfaria <rodolfo.dsfaria@gmail.com>
2015-11-10 22:33:31 +0000

    alter

--- index.php
+++ index.php
@@ -7,8 +7,13 @@ $app->get('/', function() use ( $app ) {
     echo "welcome to REST API";
 });
 
-$app->get('/hello/:name', function($name) use ( $app ) {
-    echo "Hi $name, welcome to the REST API";
+$app->get('/tasks', function($name) use ( $app ) {
+    //
+    $tasks[]= arroy(
+        array('id'=>1, 'description'=>'Learn REST', 'done'=> false),
+        array('id'=>2, 'description'=>'Learn JavaScript', 'done'=>false)
+        );
+        echo json_encode($tasks);
 });
 
 $app->run();
